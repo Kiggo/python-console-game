8897aa54b0a93173cd833b964342383b48fe29dd
Kiggo <w2nd33@naver.com>
2024-07-16 00:56:30 +0900

    Add main, Update character

--- character.py
+++ character.py
@@ -1,7 +1,7 @@
 # [캐릭터]
 # - 이름
-# - 스킬 
-# - 장비(착용부위)
+# - 스킬 - DoubleAttack, Freeze 
+# - 장비(착용부위) Weapon, Helmet, Chestplate, Shield, Boots  
 # - 레벨
 # - hp 
 # - mp 
@@ -9,3 +9,26 @@
 # - 물리방어력
 # - 마법방어력
 # - 우선순위(같으면랜덤)
+
+import random
+
+class Character:
+
+    skill = ['DoubleAttack', 'Freeze']
+
+    def __init__ (self, name):
+        self.name = name
+        self.skill = random.choice(self.skill)
+        self.weapon = ''
+        self.armor = []
+        self.level = 1
+        self.hp = 10
+        self.mp = 10
+        self.power = 10
+        self.physical_defence = 10
+        self.magic_defence = 10
+        print()
+        print(f'{name} 캐릭터가 생성되었습니다')
+        print(f'스킬은 {self.skill} 을 부여 받았습니다')
+        print(f'캐릭터 레벨은 {self.level} 이며')
+        print(f'기본 hp는 {self.hp} mp는 {self.mp}, 공격력은 {self.power} 물리방어력은 {self.physical_defence} 마법방어력은 {self.magic_defence} 입니다')
---
+++ main.py
@@ -0,0 +1,14 @@
+from character import Character
+
+# 쓰레드를 사용 해야 함
+
+# 1. 캐릭터 생성 - 사용자 입력
+character_name = input('\n캐릭터명을 입력하세요\n')
+Character(character_name)
+
+# 2. 정보 save.json에 저장
+
+# 3. 마을 또는 사냥터 이동
+
+# 4. 전투
+
